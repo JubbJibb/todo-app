9b05bb1b4eb7b8f40af9d7fd50efbf57514720b0
Piyaporn Kurusathianpong <jubbjibb@odds.team>
2026-02-13 19:01:21 +0700

    Create/ update/ Delete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,47 @@ class TodosController < ApplicationController
     @name = "Jib"
     @todos = Todo.all
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  # def create
+  #    @todo = Todo.new(todo_params)
+  #    if @todo.save
+  #     redirect_to todos_path
+  #    else
+  #     render :new
+  #    end
+  # end
+
+  def create
+    todo = Todo.new(todo_params)
+    todo.save
+    redirect_to todos_index_path
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+    redirect_to todos_index_path
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update(todo_params)
+    redirect_to todos_index_path
+  end
+  
+  private
+  def todo_params
+    params.expect(todo: [ :title ])
+  end
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 
 Rails.application.routes.draw do  
-  resources :todos  
+#  resources :todos 
+  get 'todos/index',  to: 'todos#index'
+  get 'todos/new', to: 'todos#new', as: 'new_todo'
+  delete "/todos/:id", to: "todos#destroy", as: "todo" 
+  post "/todos", to: "todos#create"
+  get "/todos/:id/edit", to: "todos#edit", as: "edit_todo"
+  patch "/todos/:id", to: "todos#update"
   root 'todos#index'
 end
